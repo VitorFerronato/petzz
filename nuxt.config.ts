46872791cbcf3ee7ce679cd535a05a058f28b8c4
Vitor Eduardo Ferronato <vitor.ferronato@neogrid.com>
2024-09-08 16:56:47 -0300

    feat: initialize project

---
+++ nuxt.config.ts
@@ -0,0 +1,55 @@
+import { auth } from "firebase-functions";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
+    "@vueuse/nuxt",
+    "@nuxt/icon",
+    "nuxt-vuefire",
+  ],
+
+  tailwindcss: {
+    exposeConfig: true,
+  },
+
+  colorMode: {
+    classSuffix: "",
+  },
+
+  typescript: {
+    shim: false,
+  },
+
+  imports: {
+    imports: [
+      {
+        from: "tailwind-variants",
+        name: "tv",
+      },
+      {
+        from: "tailwind-variants",
+        name: "VariantProps",
+        type: true,
+      },
+    ],
+  },
+
+  vuefire: {
+    auth: {
+      enabled: true,
+      sessionCookie: true,
+    },
+    config: {
+      apiKey: process.env.apiKey,
+      authDomain: process.env.authDomain,
+      projectId: process.env.projectId,
+      storageBucket: process.env.storageBucket,
+      messagingSenderId: process.env.messagingSenderId,
+      appId: process.env.appId,
+    },
+  },
+});
