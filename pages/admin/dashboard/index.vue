25c4829e49b9c2cc299465591850f4f75c6446a9
vferronato <vitorferronato@neogrid.com>
2024-09-10 22:13:20 -0300

    checkpoint

--- pages/admin/dashboard/index.vue
+++ pages/admin/dashboard/index.vue
@@ -1,5 +1,18 @@
 <template>
-  <div>welcome</div>
+  <div>
+    <UiNavbar sticky>
+      <UiContainer>
+        <NuxtLink to="/admin/dashboard">Petzz</NuxtLink>
+        <div>
+          <UiButton size="icon-sm" variant="ghost" title="Toggle theme">
+            <span class="sr-only">Toggle theme</span>
+            <Icon name="lucide:sun-medium" />
+          </UiButton>
+        </div>
+      </UiContainer>
+    </UiNavbar>
+    <h1>Welcome back {{ user?.displayName }}</h1>
+  </div>
 </template>
 
 <script setup>
